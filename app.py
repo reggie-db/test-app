380f7f710a8e18c33c985fbf63a5f170dccaacfa
reggie-db <reggie.pierce@databricks.com>
2025-10-07 00:47:41 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message = "Hello, world!"
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(message.encode("utf-8"))
+
+if __name__ == "__main__":
+    port = int(os.getenv("DATABRICKS_APP_PORT", "8000"))
+    server = HTTPServer(("0.0.0.0", port), HelloHandler)
+    print(f"Serving on port {port}")
+    server.serve_forever()
